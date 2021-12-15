60762827d8a124d68d1bf74d514e4d1976548ce8
Abhisek Joshi <nofursad@gmail.com>
2021-12-15 22:00:59 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,81 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Importing the library to use in system
+"""
+import sys
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPE_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SHEET = GSPREAD_CLIENT.open('Networking')
+
+
+def newscreen():
+    """
+    newscreening the screen for new menu or sub-menu
+    """
+    print('\033c')
+
+
+def main():
+    """
+    Main Menu to display to user at the main screen when the system loads
+    """
+    while True:
+        newscreen()
+        header()
+        print('Welcome to CLI Networking:')
+        print("Main Menu\n")
+        print("1. Login")
+        print("2. New User")
+        print("3. Exit")
+
+        choice = input("Please choose an option by entering 1, 2 or 3\n").strip()
+
+        if choice == '1':
+            login()
+        elif choice == '2':
+            createuser()
+        elif choice == '3':
+            sys.exit(0)
+        else:
+            input("Invalid selection. Please enter 1, 2, or 3\nPress Enter to continue...\n")
+
+
+def header():
+    """
+    This function print the header in the main page to welcome the
+    user when user first open the system.
+    """
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    time.sleep(0.2)
+    print("@@                                                                                                                 @@")
+    time.sleep(0.2)
+    print("@@      @@@@  @@       @@@@@@      @@      @@ @@@@@@@@@ @@@@@@@@@@ @@        @@      @@@      @@@@@@    @@    @@   @@")
+    time.sleep(0.2)
+    print("@@     @@     @@         @@        @@@     @@ @@            @@     @@        @@     @@ @@     @@   @@   @@   @@    @@")
+    time.sleep(0.2)
+    print("@@   @@       @@         @@        @@ @@   @@ @@            @@     @@        @@   @@     @@   @@   @@   @@ @@      @@")
+    time.sleep(0.2)
+    print("@@   @@       @@         @@        @@  @@  @@ @@@@@@        @@     @@   @@   @@  @@       @@  @@@@@@    @@@@       @@")
+    time.sleep(0.2)
+    print("@@   @@       @@         @@        @@   @@ @@ @@            @@     @@  @@@@  @@   @@     @@   @@ @@     @@ @@      @@")
+    time.sleep(0.2)
+    print("@@     @@     @@         @@        @@     @@@ @@            @@     @@@@    @@@@     @@ @@     @@   @@   @@   @@    @@")
+    time.sleep(0.2)
+    print("@@      @@@@  @@@@@@@  @@@@@@      @@      @@ @@@@@@@@@     @@     @@@      @@@      @@@      @@    @@  @@    @@   @@")
+    time.sleep(0.2)
+    print("@@                                                                                                                 @@")
+    time.sleep(0.2)
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+
+if __name__ == "__main__":
+    main()
